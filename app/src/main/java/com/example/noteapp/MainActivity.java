bb2119ad4c735c853f2c72376422134a10a7caa8
al3xb0 <alikon1@st.amu.edu.pl>
2023-10-08 18:04:39 +0200

    main layout design and notes functions

--- app/src/main/java/com/example/noteapp/MainActivity.java
+++ app/src/main/java/com/example/noteapp/MainActivity.java
@@ -2,13 +2,63 @@ package com.example.noteapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String PREFS_NAME = "NotePrefs";
+    private static final String KEY_NOTE_COUNT = "NoteCount";
+    private LinearLayout notesContainer;
+    private List<Note> noteList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        notesContainer = findViewById(R.id.notesContainer);
+        Button saveButton = findViewById(R.id.saveButton);
+
+        noteList = new ArrayList<>();
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveNote();
+            }
+        });
+
+    }
+
+    private void saveNote() {
+        EditText titleEditText = findViewById(R.id.titleEditText);
+        EditText contentEditText = findViewById(R.id.contentEditText);
+
+        String title = titleEditText.getText().toString();
+        String content = contentEditText.getText().toString();
+
+        if (!title.isEmpty() && !content.isEmpty()){
+            Note note = new Note();
+            note.setTitle(title);
+            note.setContent(content);
+
+            noteList.add(note);
+            saveNotesToPreferences();
+        }
+    }
+
+    private void saveNotesToPreferences() {
+        SharedPreferences sharedPreferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putInt(KEY_NOTE_COUNT, noteList.size());
     }
 }
