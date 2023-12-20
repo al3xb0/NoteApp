3dba126abb1793961ce5e7e7d654e678f20084ee
al3xb0 <alikon1@st.amu.edu.pl>
2023-12-20 11:25:09 +0100

    prepared to sign hash(will be continued)

--- app/src/main/java/com/example/noteapp/ChangePasswordActivity.java
+++ app/src/main/java/com/example/noteapp/ChangePasswordActivity.java
@@ -1,14 +1,13 @@
 package com.example.noteapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
--- app/src/main/java/com/example/noteapp/HashHelper.java
+++ app/src/main/java/com/example/noteapp/HashHelper.java
@@ -4,28 +4,44 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+
 public class HashHelper {
 
     public static String hashFunction(String string) {
         try {
+
+//            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+//            keyPairGenerator.initialize(2048);
+//            KeyPair keyPair = keyPairGenerator.generateKeyPair();
+//            PrivateKey privateKey = keyPair.getPrivate();
+//            PublicKey publicKey = keyPair.getPublic();
+
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
             byte[] hashBytes = digest.digest(string.getBytes(StandardCharsets.UTF_8));
 
+//            Signature signature = Signature.getInstance("SHA256withRSA");
+//            signature.initSign(privateKey);
+//            signature.update(hashBytes);
+//            byte[] signedHash = signature.sign();
 
             StringBuilder hexStringBuilder = new StringBuilder();
             for (byte b : hashBytes) {
                 hexStringBuilder.append(String.format("%02x", b));
             }
+
             return hexStringBuilder.toString();
 
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
             return null;
         }
+
     }
+
 
     public static boolean verifyHash(String enteredString, String storedHashedString) {
         String enteredPasswordHash = hashFunction(enteredString);
         return enteredPasswordHash != null && enteredPasswordHash.equals(storedHashedString);
     }
+
 }
